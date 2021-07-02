c15cf5b4e13fa8104e62d4af5da43fc7c6b1e6a3
QinYUN575 <QinYUN575@Gmail.com>
2021-07-02 08:34:00 +0800

    Add rt-thread typedef

--- applications/main.c
+++ applications/main.c
@@ -1,3 +1,14 @@
+/**
+ * @file main.c
+ * @author TsMax (QinYUN575@Foxmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2021-07-02
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
 
 int main()
 {
---
+++ applications/rtconfig.h
@@ -0,0 +1,16 @@
+/**
+ * @file rtconfig.h
+ * @author TsMax (QinYUN575@Foxmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2021-07-02
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+#ifndef __RT_CONFIG_H__
+#define __RT_CONFIG_H__
+
+#define RT_ALIGN_SIZE       4
+
+#endif
